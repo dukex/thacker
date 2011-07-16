ae11ca8dafccbfa6662b79c37ad9c0122c775337
Emerson Vinicius <duke@riseup.net>
2011-07-15 23:48:01 -0300

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thacker::Application.config.secret_token = 'f254b68d4ec0a3c01611943f8bec5d920f97fde852698be5ae8176ccb2e45a8ad82c74cf6d64a726a590c76119032e7664ff5cf5f7b0a99abe6e5041b58ae054'
