60a18a855f85e48e944c656dac1f576b7ffaf130
Emerson Vinicius <duke@riseup.net>
2011-07-16 19:04:07 -0300

    added factory for livestreams

--- spec/support/factories/livestream.rb
+++ spec/support/factories/livestream.rb
@@ -1,2 +1,16 @@
 Factory.define :live_stream do |f|
 end
+
+
+Factory.define :user_live_stream, :parent => :live_stream, :class => "UserLiveStream" do |f|
+  f.association :user
+end
+
+Factory.define :post_live_stream, :parent => :live_stream, :class => "PostLiveStream" do |f|
+  f.association :user
+  f.association :post
+end
+
+Factory.define :project_live_stream, :parent => :live_stream, :class => "ProjectLiveStream" do |f|
+end
+
