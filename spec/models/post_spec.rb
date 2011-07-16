3068aa60201f99790a9bd0d7f0ed1e13d817eb1b
Emerson Vinicius <duke@riseup.net>
2011-07-16 17:08:27 -0300

    validate presence of title, body and use to post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,8 @@
 require 'spec_helper'
 
 describe Post do
+  should_belong_to :user
+  should_validate_presence_of :title
+  should_validate_presence_of :body
+  should_validate_presence_of :user
 end
