8fef9114b0e51eff16a742fc297e43cb44311dfc
Emerson Vinicius <duke@riseup.net>
2011-07-16 14:24:27 -0300

    set user on post create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,12 @@
 class PostsController < InheritedResources::Base
+  before_filter :authenticate_user!, :only => :new
+  before_filter :set_user, :only => :create
+
+
+  private
+
+  def set_user
+    params[:post][:user] = current_user
+  end
+
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,4 +1,12 @@
 require 'spec_helper'
 
 describe PostsController do
+  with_a_logged_user do
+    describe "POST create" do
+      it "should set user as current user" do
+        post :create, :post => { :title => "Ola", :body => "Mundo"}
+        Post.last.user.should == user
+      end
+    end
+  end
 end
