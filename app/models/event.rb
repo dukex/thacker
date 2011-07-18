307da168a7d6ccb123d8ec95cee469954075239b
Emerson Vinicius <duke@riseup.net>
2011-07-17 23:02:38 -0300

    validate event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ActiveRecord::Base
+  validates_presence_of :name, :author, :about
   belongs_to :author, :class_name => "User"
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,4 +2,7 @@ require 'spec_helper'
 
 describe Event do
   should_belong_to :author, :class_name => "User"
+  should_validate_presence_of :name
+  should_validate_presence_of :author
+  should_validate_presence_of :about
 end
