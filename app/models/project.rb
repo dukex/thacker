28c339377553b7085f7ace23ba068230c8d6e602
Duke <duke@riseup.net>
2011-12-03 20:37:38 -0200

    fixed author in Project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ActiveRecord::Base
-  belongs_to :author
+  belongs_to :author, :class_name => "User"
   belongs_to :status
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
 
 describe Project do
+  should_belong_to :author, :class_name => "User"
 end
